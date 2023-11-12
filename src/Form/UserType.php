baed0325f23d3d0ed12cf6ef45e3c43b374ac6f3
vivianmartinez <128734243+vivianmartinez@users.noreply.github.com>
2023-11-12 22:48:43 +0100

    create ApiLoginController and user form type

---
+++ src/Form/UserType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username')
+            //->add('roles')
+            ->add('password')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'constraints' =>[
+                new UniqueEntity([
+                        'entityClass' => User::class,
+                        'fields' => 'username'
+                    ])
+            ]
+        ]);
+    }
+}
