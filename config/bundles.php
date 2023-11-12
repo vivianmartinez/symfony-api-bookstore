a516da2122fc93610d086c5a2666f812ad0e3bdd
vivianmartinez <128734243+vivianmartinez@users.noreply.github.com>
2023-11-12 22:39:05 +0100

    install security-bundle and create entity user

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
